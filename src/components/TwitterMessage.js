c9af0c72a5d071f3f4cbea61f6a7f61560928719
Mohammed Noori <kmanoorib@gmail.com>
2019-08-20 03:42:24 +0000

    Automatically backed up by Learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -23,7 +23,7 @@ class TwitterMessage extends React.Component {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" value={this.state.message} onChange={this.characterWatcher}/>
       </div>
     );
   }
